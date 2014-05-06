144158ccaf7655dee1a890012dbe03d1e910f816
aspick <info@aspick.net>
2014-05-07 01:37:33 +0900

    init

---
+++ APCAudioPlayer.js
@@ -0,0 +1,14 @@
+var AudioPlayer = function(audioElement,playerElement){
+	this.player = playerElement;
+	this.audio = audioElement;
+}
+
+AudioPlayer.prototype.build = function(){
+	// contains single page jQuery objects
+	var player = this.player;
+	var audio = this.audio;
+	var _this = this;
+
+	
+
+}
